51aea74e5c3d16cacf94768e655e72133c3c5a24
Phibi1020 <foongjing5@gmail.com>
2021-09-29 18:16:59 +0800

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -11,13 +11,12 @@ char	*ft_strjoin(const char *s1, const char *s2)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (s2[j] != '\0')
+	while (s2[j])
 	{
 		str[i + j] = s2[j];
 		j++;
