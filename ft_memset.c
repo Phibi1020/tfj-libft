a86c047cfd945d2c4776f38926a0602e3d0614c0
Phibi1020 <foongjing5@gmail.com>
2021-10-02 22:42:01 +0800

    check

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: ftan <ftan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 20:22:31 by ftan              #+#    #+#             */
-/*   Updated: 2021/10/02 22:37:08 by ftan             ###   ########.fr       */
+/*   Updated: 2021/10/02 22:39:17 by ftan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memset(void *str, int c, size_t n)
 	iter = -1;
 	while (++iter < n)
 	{
-		((unsigned char)str)[iter] = c;
+		((unsigned char *)str)[iter] = (unsigned char)c;
 	}
 	return (str);
 }
